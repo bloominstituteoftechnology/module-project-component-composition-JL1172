76257413cbc09fb6187aa66de667b16fe566c680
Jacob Lang <jacoblang72@comcast.net>
2023-07-27 03:13:42 -0400

    almostthere

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Image from './Image';
 
 
-const url = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+
 const imgStyle = {
   width : '1500px',
   height : '950px'
@@ -12,18 +12,27 @@ const imgStyle = {
 
 function App() {
   const [data,setData] = useState([]);
-  const [date,setDate] = useState('')
+  const [date,setDate] = useState('');
+  let year = parseInt(date.slice(0, 4));
+  let month = parseInt(date.slice(4, 6));
+  let day = parseInt(date.slice(6, 8));
+
+
+  const url = date.length !== 8 ? "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY" 
+  : `https://api.nasa.gov/planetary/apod?date=${year}-${month}-${day}`;
+
+  const secondUrl = date.length !== 8 ? "http://localhost:9009/api/apod?api_key=DEMO_KEY" 
+  : `http://localhost:9009/api/apod?date=${year}-${month}-${day}` ;
 
   useEffect(() => {
   
        axios.get(url)
       .then(res => {
        setData(res.data)
-        console.log(res) 
       }) 
       .catch(err=> {
         console.log('The first one did not work')
-         axios.get("http://localhost:9009/api/apod?api_key=DEMO_KEY")
+         axios.get(secondUrl)
         .then(res=> {
           setData(res.data)
           console.log(res.data)
@@ -52,7 +61,7 @@ function App() {
   return (
   <>
   <div style = {{margin : '2rem'}}> 
-    <Image date = {date} setDate = {setDate} data = {data} imgStyle = {imgStyle} />
+    {data && <Image date = {date} setDate = {setDate} data = {data} imgStyle = {imgStyle} />}
   </div>
   </>
   )
--- frontend/components/SearchDate.js
+++ frontend/components/SearchDate.js
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function SearchDate(props) {
-    const {date,setDate} = props;
+    const { date, setDate } = props;
+
+    let dateCopy = date;
+    let newDateCopy = dateCopy.replace(/-/g, '');
+    let newArray = [];
+    let array = newDateCopy.split('');
+    let numberArray = array.map(n => {
+        const num = +n;
+        newArray.push(num);
+        return newArray;
+    });
+    let year = newArray.join('').slice(0, 4);
+    let month = newArray.join('').slice(4, 6);
+    let day = newArray.join('').slice(6, 8);
+    let currentYear = new Date().getFullYear();
+
+
+    let secondTernary = year > currentYear ?
+        <label>Must pic a year in the past</label> :
+        month > 12  || day > 31 ? 
+        <label>Must be a valid date</label> :
+        '';
+
+    let lengthTernary =  newArray.length > 8  ?
+    <label>Date must be proper length</label> : '';
+    // let monthComparison = 
+    // const filteredArray = numberArray.filter(n=> typeof n !== "number");
+    // let numberCondition =  filteredArray.length;
+    // let ternary = valueConditionLength > 0 
+    // ? <label>Value must be less than 10</label> 
+    // : numberCondition !== 0 
+    // ? <label>Value must be a number</label> 
+    // : ''; 
+
+
+
     const changeHandler = (evt) => {
-        setDate(evt.target.value)
+        setDate(evt.target.value);
     }
     return (
         <>
-        <div id = 'label'>
-            <label>Type in data in this format :  </label>
-            <input type="text" placeholder="YYYY-MM-DD" 
-            onChange={changeHandler}></input>
-        </div>
+            <div id='label'>
+                <label>Type in data in this format :  </label>
+                <input type="text" placeholder="YYYY-MM-DD"
+                    onChange={changeHandler}></input>
+                <div>
+                    {secondTernary}
+                </div>
+                <div>
+                    {lengthTernary}
+                </div>
+            </div>
         </>
     )
 }
